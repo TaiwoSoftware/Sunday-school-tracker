8797b06ed31e97a734bd4a753da758a84ed6ade1
Taiwo Akerele <davcodes24@gmail.com>
2024-06-12 20:58:36 -0700

    add all

--- src/components/TimeDate.jsx
+++ src/components/TimeDate.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 export default function TimeDate() {
   const [dateTime, setDateTime] = useState(null);
